fb4336ff9f9fdc3d07ad2e2d021fb55cd3ba2906
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2025-10-08 11:01:13 -0700

    Bump version: 0.3.1 -> 0.3.2

--- src/atheon_codex/__init__.py
+++ src/atheon_codex/__init__.py
@@ -2,7 +2,7 @@ from .async_client import AsyncAtheonCodexClient
 from .client import AtheonCodexClient
 from .models import AdUnitsFetchModel, AdUnitsIntegrateModel
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __all__ = [
     "AsyncAtheonCodexClient",
     "AtheonCodexClient",
